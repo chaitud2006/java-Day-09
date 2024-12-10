52f5f6e17b9af4d572a43c183db2e62bdcb3f737
chaitud2006 <cc2214574@gmail.com>
2024-12-10 14:24:02 +0530

    Add files via upload

---
+++ For_each.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class For_each
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Orange","Pineapple"};
+        for(String b:a)
+        System.out.println(b+" ");
+    }
+}
